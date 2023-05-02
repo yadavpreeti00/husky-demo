adaf47114a39515b51ffcdf7172110b1503d1b35
yadavpreeti00 <preeti.yadav@kickdrumtech.com>
2023-05-02 12:54:56 +0530

    testing color variables

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload and .
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn react
+          Learn reactA
         </a>
       </header>
     </div>
