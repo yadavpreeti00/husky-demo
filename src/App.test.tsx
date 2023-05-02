c87998a5fd3d5ef61e7820d0ce55607582384c8e
yadavpreeti00 <preeti.yadav@kickdrumtech.com>
2023-05-02 12:56:58 +0530

    checking

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn reactA/i);
+  const linkElement = screen.getByText(/learn reaA/i);
   expect(linkElement).toBeInTheDocument();
 });
